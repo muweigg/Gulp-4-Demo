4d542e6357fd5657c83d7d9c4a95cb4254ce6ca7
[muweigg] <[muweigg@gmail.com]>
2017-12-08 17:48:41 +0800

    update

--- src/js/common/vendors.ts
+++ src/js/common/vendors.ts
@@ -3,3 +3,8 @@ import 'script-loader!babel-loader!lodash/lodash';
 import 'script-loader!babel-loader!vue/dist/vue.min';
 import 'script-loader!babel-loader!axios/dist/axios';
 import 'script-loader!babel-loader!jquery/dist/jquery.slim';
+
+window.isIE = () => {
+    let match = navigator.userAgent.match(/(?:MSIE |Trident\/.*; rv:)(\d+)/);
+    return match ? parseInt(match[1]) : undefined;
+}
--- src/js/global.d.ts
+++ src/js/global.d.ts
@@ -1,1 +1,6 @@
 declare var Rx: any;
+declare function isIE(): number | undefined;
+
+interface Window {
+    isIE(): number | undefined;
+}
